b831f4b9a8f743932033b5f9ee060aca036a7248
javi <enriq018coding@gmail.com>
2018-05-03 00:52:17 -0700

    remove .env from commit history

--- server/index.js
+++ server/index.js
@@ -5,13 +5,12 @@ const express = require('express');
 const path = require('path');
 
 const { analyzeTweets } = require('./analyzeTweets.js');
+
 const port = process.env.PORT;
 
 const app = express();
 
-
 app.use('/', express.static(path.join(__dirname, '../client/dist')));
-
 
 app.listen(port, () => console.log(`live on port: ${port}`));
 
